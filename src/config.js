788d4b4272c29db090a9d6858affb4cc04b5dc72
Five Y Technologies <142930108+Win-Win-Tech@users.noreply.github.com>
2025-10-26 13:24:06 +0530

    26-Oct

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    apiUrl: "http://localhost:3002"
-    // apiUrl: "https://apialagar.cloudgentechnologies.com"
+    //apiUrl: "http://localhost:3002"
+      apiUrl: "http://147.93.27.224:3003"
       
 }
 export default config;
